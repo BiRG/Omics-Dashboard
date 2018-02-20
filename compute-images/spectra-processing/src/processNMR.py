164dcc4cadfe76bbbb1b3d7b6c1f0f4368e41214
Daniel Foose <dpfoose@gmail.com>
2018-02-20 13:54:04 -0500

    made NMR processing CWL CLT work in Docker

--- compute-images/spectra-processing/src/processNMR.py
+++ compute-images/spectra-processing/src/processNMR.py
@@ -3,12 +3,14 @@ import numpy as np
 import shutil
 import sys
 import NMRproc
+import os
 
 filename = sys.argv[1]
 frequency = np.float64(sys.argv[2])
 reference = np.float64(sys.argv[3])
 x_window = np.float64(sys.argv[4])
-shutil.copy(filename, 'out.h5')
+outfilename = f'{os.environ["HOME"]}/out.h5'
+shutil.copy(filename, outfilename)
 
-NMRproc.scale('out.h5', frequency)
+NMRproc.scale(outfilename, frequency)
 # NMRproc.reference('out.h5', reference, x_window) # this breaks some workflows
