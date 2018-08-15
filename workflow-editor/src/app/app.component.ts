f3ace33a76b511da2379a6e4812763ba0383087b
Daniel Foose <dpfoose@gmail.com>
2018-08-15 15:46:14 -0400

    Upgraded angular-cli

--- workflow-editor/src/app/app.component.ts
+++ workflow-editor/src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {WorkflowFactory} from 'cwlts/models';
+import {Workflow} from 'cwl-svg';
+import {SVGArrangePlugin} from 'cwl-svg';
+import {SVGNodeMovePlugin} from 'cwl-svg';
+import {SVGPortDragPlugin} from 'cwl-svg';
+import {SelectionPlugin} from 'cwl-svg';
+import {SVGEdgeHoverPlugin} from 'cwl-svg';
+import {ZoomPlugin} from 'cwl-svg';
 
 @Component({
   selector: 'app-root',
