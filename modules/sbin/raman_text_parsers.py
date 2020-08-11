40ee39adb519f54e908288e5843e74ac457180a3
Daniel Foose <dpfoose@gmail.com>
2020-08-11 00:25:15 -0400

    make it easier for prefixes other than /omics

--- modules/sbin/raman_text_parsers.py
+++ modules/sbin/raman_text_parsers.py
@@ -1,6 +1,7 @@
 import numpy as np
 import h5py
 import io
+import os
 
 
 def parse_wide_map(filename):
@@ -22,19 +23,19 @@ def parse_paired_text(filename):
 def save_map_sample_file(in_filename, out_filename, prefix):
     x, Y, spatial_x, spatial_y = parse_wide_map(in_filename) 
     with h5py.File(out_filename, 'w') as out_file:
-        out_file.create_dataset('x', data=x)
+        out_file.create_dataset('x', data=x.reshape(1, -1))
         out_file.create_dataset('Y', data=Y)
-        out_file.create_dataset('spatial_x', data=spatial_x)
-        out_file.create_dataset('spatial_y', data=spatial_y)
-        out_file.attrs['name'] = f'{prefix}: {os.path.basename(in_filename)}'
-        out_file.attrs['filename'] = os.path.basename(in_filename)
+        out_file.create_dataset('spatial_x', data=spatial_x.reshape(-1, 1))
+        out_file.create_dataset('spatial_y', data=spatial_y.reshape(-1, 1))
+        out_file.attrs['name'] = np.string_(f'{prefix}: {os.path.basename(in_filename)}')
+        out_file.attrs['filename'] = np.string_(os.path.basename(in_filename))
 
 
 def save_point_sample_file(in_filename, out_filename, prefix):
     x, Y = parse_paired_text(in_filename)
     with h5py.File(out_filename, 'w') as out_file:
-        out_file.create_dataset('x', data=x)
+        out_file.create_dataset('x', data=x.reshape(1, -1))
         out_file.create_dataset('Y', data=Y)
-        out_file.attrs['name'] = f'{prefix}: {os.path.basename(in_filename)}'
-        out_file.attrs['filename'] = os.path.basename(in_filename)
+        out_file.attrs['name'] = np.string_(f'{prefix}: {os.path.basename(in_filename)}')
+        out_file.attrs['filename'] = np.string_(os.path.basename(in_filename))
 
