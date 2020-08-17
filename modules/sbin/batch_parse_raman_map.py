42611b02d9edcfd256eafe913aadda2e43773c55
Daniel Foose <dpfoose@gmail.com>
2020-08-17 01:31:20 -0400

    ignoring manifest.csv for bulk Raman spectra import

--- modules/sbin/batch_parse_raman_map.py
+++ modules/sbin/batch_parse_raman_map.py
@@ -2,7 +2,7 @@
 import sys
 import raman_text_parsers as tp
 import os
-in_filenames = sys.argv[1:len(sys.argv)-1]
+in_filenames = [f for f in sys.argv[1:len(sys.argv)-1] if f != 'manifest.csv']
 name_prefix = sys.argv[len(sys.argv)-1]
 for in_filename in in_filenames:
     out_filename = f'{os.environ["HOME"]}/{os.path.basename(in_filename)}.h5'
